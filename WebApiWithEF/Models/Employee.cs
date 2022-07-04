01cb63ad859952694e2ba603e1e4d607b5359181
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-07-04 15:47:22 +0100

    ADD seed data

--- WebApiWithEF/Models/Employee.cs
+++ WebApiWithEF/Models/Employee.cs
@@ -16,7 +16,7 @@ namespace WebApiWithEF.Models
         public string EmployeeName { get; set; }
         public string DateOfBirth { get; set; }
         public string Gender { get; set; }
-        public string CurrentAdress { get; set; }
+        public string CurrentAddress { get; set; }
         public string PermanentAddress { get; set; }
         public string City { get; set; }
         public string Nationality { get; set; }
