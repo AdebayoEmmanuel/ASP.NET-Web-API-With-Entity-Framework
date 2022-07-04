3120e0788893fb171a08b37a841a79c72cf7f1c0
Emmanuel Adebayo <adebayodemilade7@gmail.com>
2022-07-04 15:23:47 +0100

    Currently runnning checks to ensure DB connects

---
+++ WebApiWithEF/Context/CompanyContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiWithEF.Context
+{
+    using Microsoft.EntityFrameworkCore;
+    using WebApiWithEF.Models;
+    public class CompanyContext
+        : DbContext
+    {
+        public CompanyContext(DbContextOptions options)
+            : base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
